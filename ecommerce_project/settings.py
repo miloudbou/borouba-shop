9ddf7e6ed6b1b903051a05ecd983f098fd3caa40
miloud bourouba <miloud400@yahoo.com>
2025-04-29 16:51:50 +0200

    Fix CSRF settings for production

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -16,6 +16,7 @@ DATABASES = {
 DATABASES['default']['OPTIONS'] = {
     'sslmode': 'require',
 }
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(",")
 # إعدادات AliExpress API
 ALIEXPRESS_API_URL = "https://api-sandbox.aliexpress.com"
 ALIEXPRESS_APP_KEY = "ضع_المفتاح_هنا"
